5dddc2e28adae47e815546476b31135946d5b4a4
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 06:13:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO users (id, name, age) VALUES (1, 'Cassie', 17), (2, 'Humpty', 6), (3
 (5, 'Adam', 24), (6, 'Susan', 24), (7, 'Rufus', 9), (8, 'Victor', 23), (9, 'Frank', 100), (10, 'Helga', 30),
 (11, 'Sbuffles', 90), (12, 'Mrie', 24), (13, 'Stormzy', 4), (14, 'Celine', 36), (15, 'Albi', 113), (16, 'Squad', 5),
 (17, 'Dolphin', 6), (18, 'Polka', 5), (19, 'Ely', 24), (20, 'Kate', 24);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
