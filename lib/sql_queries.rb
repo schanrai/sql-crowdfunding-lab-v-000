b9a66285886a6400e7353882212a97774ae6c18d
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 08:30:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY title
-HAVING SUM(pledges.amount) => projects.funding_goal;"
+HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
