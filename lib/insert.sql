f8a021a3f9b65a11aa7337542cd12ae62f6ddfc9
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 06:12:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,4 @@
-INSERT INTO users (id, name, age) VALUES 
+INSERT INTO users (id, name, age) VALUES (1, 'Cassie', 17), (2, 'Humpty', 6), (3, 'Lizard', 4), (4, 'Alexis', 33), 
+(5, 'Adam', 24), (6, 'Susan', 24), (7, 'Rufus', 9), (8, 'Victor', 23), (9, 'Frank', 100), (10, 'Helga', 30), 
+(11, 'Sbuffles', 90), (12, 'Mrie', 24), (13, 'Stormzy', 4), (14, 'Celine', 36), (15, 'Albi', 113), (16, 'Squad', 5), 
+(17, 'Dolphin', 6), (18, 'Polka', 5), (19, 'Ely', 24), (20, 'Kate', 24);
