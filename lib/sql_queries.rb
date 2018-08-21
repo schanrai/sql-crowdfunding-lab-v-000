49eb1070250837339e7ba557238e526b0249deee
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 08:54:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,12 @@ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT name, SUM(pledges.amount)
+FROM users
+INNER JOIN pledges
+ON pledges.user_id = users.id
+GROUP BY name
+ORDER BY SUM(pledges.amount), name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
