ec03d53be8422eca664e90f9b12b5deeebc13bdb
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 08:24:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-WHERE pledges.amount > projects.funding_goal
+WHERE SUM(pledges.amount) > projects.funding_goal
 GROUP BY title;"
 end
 
