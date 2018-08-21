32c74bab58ce49b967a4e1702bdb8a140c653547
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 07:18:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT title, pledges.amount
+FROM projects
+INNER JOIN cats_owners
+ON owners.id = cats_owners.owner_id WHERE cats_owners.cat_id = 3;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
