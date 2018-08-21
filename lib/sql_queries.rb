22910310c78d26d9720412511e3223c2aae8f9e2
Sushi Chanrai <sushi@pathworks.co>
2018-08-21 08:02:31 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,8 +18,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT users.name, users.age, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
-ON pledges.project_id = projects.id
-GROUP BY title;"
+ON pledges.user_id = user.id
+GROUP BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
